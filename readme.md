8316ba9d3cb14e892682a172822fc87f43c88f94
hacker <hacker@hackers-MacBook-Pro.local>
2025-04-20 18:58:26 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Toolkit
+
+A simple example of how to create a reusable Go module with commonly used tools.
+
+The includes tools are:
+
+- [✅] Read json
+- [✅] Read json
+- [✅] Read json
+- [❌] Read json
+- [❌] Read json
+- [❌] Read json
+- [❌] Read json
+- [❌] Read json
+
+## Installation
+
+`go get -u github.com/tennessine/toolkit`
--- tools.go
+++ tools.go
@@ -2,11 +2,14 @@ package toolkit
 
 import (
 	"crypto/rand"
+	"errors"
+	"net/http"
 )
 
 const randStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789_+"
 
 type Tools struct {
+	MaxFileSize int
 }
 
 func (t *Tools) RandomString(n int) string {
@@ -17,4 +20,27 @@ func (t *Tools) RandomString(n int) string {
 		s[i] = r[x%y]
 	}
 	return string(s)
+}
+
+type UploadedFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int
+}
+
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+	var uploadedFiles []*UploadedFile
+
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024
+	}
+
+	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+	if err != nil {
+		return nil, errors.New("the uploaded file is too big")
+	}
 }
